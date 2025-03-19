b30d0a09e3656bf746d4c07178c4b4a9b1abe2b4
Maria Eduarda da Nóbrega <eduardanbg2005@gmail.com>
2025-03-19 20:37:32 -0300

    Adicionando o CSV no Java.

---
+++ src/LerArquivosCsv.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LerArquivosCsv {
+    public static void main(String[] args) {
+        String cvsArquivo = "C:\\Users\\eduar\\Documents\\UEPB - 4º Período\\LEDA\\Projeto-LEDA\\passwords.csv";
+    }
+}
+
